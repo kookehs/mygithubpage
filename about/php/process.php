6a952b35ec340312e927ec3d0cfb8ea1160a62c6
alprusa <aprusa@ucsc.edu>
2016-02-25 02:40:04 -0800

    Forum appearance fixed no idea if it works though no more bugs though

--- about/php/process.php
+++ about/php/process.php
@@ -1,52 +1,78 @@
 <?php
-//Contact form php file check if form was submited
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    //variables needed to work with input values
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $human = intval($_POST['human']);
+$name = $_POST["name"];
+$subject = $_POST["subject"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+$answer = intval($_POST["answer"]);
+ 
+$EmailTo = "panopticon@whos-watching.me";
 
-    //variables needed to for sending the email
-    $from = 'User email'; 
-    $to = 'panopticon@whos-watching.me'; 
-    $subject = 'Message from user ';
-     
-    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+$errorMSG = "";
+ 
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
 
-    //make sure user entered a name
-    if (!$_POST['name']) {
-        $errName = 'Please enter your name';
-    }
+// Subject
+if (empty($_POST["subject"])) {
+    $errorMSG = "Subject is required ";
+} else {
+    $name = $_POST["subject"];
+}
 
-    //make sure user entered a subject
-    if (!$_POST['subject']) {
-        $errSubject = 'Please enter a subject';
-    }
+// EMAIL
+if (empty($_POST["email"]) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    $errorMSG .= "Email is missing or incorrect ";
+} else {
+    $email = $_POST["email"];
+}
+ 
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "Message is required ";
+} else {
+    $message = $_POST["message"];
+}
 
-    //make sure user entered an email and if it is valid
-    if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-        $errEmail = 'Please enter a valid email address';
-    }
+// Answer
+if ($human !== 5) {
+    $errorMSG .= "Value is incorrected ";
+} else {
+    $message = $_POST["answer"];
+}
+ 
+// prepare email body text
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
 
-    //make sure user entered a message
-    if (!$_POST['message']) {
-        $errMessage = 'Please enter your message';
-    }
-
-    //make sure the user is not a robot
-    if ($human !== 5) {
-        $errHuman = 'Your anti-spam is incorrect';
-    }
-    
-    //If there are no errors, send the email
-    if (!$errName && !errSubject && !$errEmail && !$errMessage && !$errHuman) {
-        if (mail ($to, $subject, $body, $from)) {
-            $result='<div class="alert alert-success">Thank You! We will be in touch</div>';
-        } else {
-            $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
-        }
+$Body .= "Subject: ";
+$Body .= $subject;
+$Body .= "\n";
+ 
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+ 
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+ 
+// send email
+$success = mail($EmailTo, $subject, $Body, "From:".$email);
+ 
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
     }
 }
+ 
 ?>
