41856028bdce19cf5f18b30bef2c73bee1bb7587
alprusa <aprusa@ucsc.edu>
2016-03-13 03:48:15 -0700

    Added proper html format to php file

--- about/php/process.php
+++ about/php/process.php
@@ -1,79 +1,86 @@
-<?php
-$name = $_POST["name"];
-$subject = $_POST["subject"];
-$email = $_POST["email"];
-$message = $_POST["message"];
-$answer = intval($_POST["answer"]);
- 
-$EmailTo = "panopticon@whos-watching.me";
+<html>
+    <head>
+        <title>Contact Form</title>
+    /head>
+    <body>
+        <?php
+        $name = $_POST["name"];
+        $subject = $_POST["subject"];
+        $email = $_POST["email"];
+        $message = $_POST["message"];
+        $answer = intval($_POST["answer"]);
+         
+        $EmailTo = "panopticon@whos-watching.me";
 
-$errorMSG = "";
- 
-// NAME
-if (empty($_POST["name"])) {
-    $errorMSG = "Name is required ";
-} else {
-    $name = $_POST["name"];
-}
+        $errorMSG = "";
+         
+        // NAME
+        if (empty($_POST["name"])) {
+            $errorMSG = "Name is required ";
+        } else {
+            $name = $_POST["name"];
+        }
 
-// Subject
-if (empty($_POST["subject"])) {
-    $errorMSG = "Subject is required ";
-} else {
-    $name = $_POST["subject"];
-}
+        // Subject
+        if (empty($_POST["subject"])) {
+            $errorMSG = "Subject is required ";
+        } else {
+            $name = $_POST["subject"];
+        }
 
-// EMAIL
-if (empty($_POST["email"]) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-    $errorMSG .= "Email is missing or incorrect ";
-} else {
-    $email = $_POST["email"];
-}
- 
-// MESSAGE
-if (empty($_POST["message"])) {
-    $errorMSG .= "Message is required ";
-} else {
-    $message = $_POST["message"];
-}
+        // EMAIL
+        if (empty($_POST["email"]) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $errorMSG .= "Email is missing or incorrect ";
+        } else {
+            $email = $_POST["email"];
+        }
+         
+        // MESSAGE
+        if (empty($_POST["message"])) {
+            $errorMSG .= "Message is required ";
+        } else {
+            $message = $_POST["message"];
+        }
 
-// Answer
-if ($human !== 5) {
-    $errorMSG .= "Value is incorrected ";
-} else {
-    $message = $_POST["answer"];
-}
- 
-// prepare email body text
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
+        // Answer
+        if ($human !== 5) {
+            $errorMSG .= "Value is incorrected ";
+        } else {
+            $message = $_POST["answer"];
+        }
+         
+        // prepare email body text
+        $Body .= "Name: ";
+        $Body .= $name;
+        $Body .= "\n";
 
-$Body .= "Subject: ";
-$Body .= $subject;
-$Body .= "\n";
- 
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
- 
-$Body .= "Message: ";
-$Body .= $message;
-$Body .= "\n";
- 
-// send email
-$success = mail($EmailTo, $subject, $Body, "From:".$email);
- 
-// redirect to success page
-if ($success && $errorMSG == ""){
-   echo "success";
-   header( "Location: http://whos-watching.me/about" );
-}else{
-    if($errorMSG == ""){
-        echo "Something went wrong :(";
-    } else {
-        echo $errorMSG;
-    }
-}
- 
-?>
+        $Body .= "Subject: ";
+        $Body .= $subject;
+        $Body .= "\n";
+         
+        $Body .= "Email: ";
+        $Body .= $email;
+        $Body .= "\n";
+         
+        $Body .= "Message: ";
+        $Body .= $message;
+        $Body .= "\n";
+         
+        // send email
+        $success = mail($EmailTo, $subject, $Body, "From:".$email);
+         
+        // redirect to success page
+        if ($success && $errorMSG == ""){
+           echo "success";
+           header( "Location: http://whos-watching.me/about" );
+        }else{
+            if($errorMSG == ""){
+                echo "Something went wrong :(";
+            } else {
+                echo $errorMSG;
+            }
+        }
+         
+        ?>
+    </body>
+</html>
